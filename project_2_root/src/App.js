5871cc0bfce159fdc116a72fa325103ea13afb08
Danny K <dxkalnz@gmail.com>
2022-10-03 17:01:27 -0600

    Clean up all files, JSON in console

--- project_2_root/src/App.js
+++ project_2_root/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+//App Object
+import { useEffect } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	//-----Fetch-----
+	// API REFERENCE - https://github.com/lukePeavey/quotable/blob/master/README.md#api-reference-
 
+	let urlCat = 'random';
+	const url = `https://api.quotable.io/${urlCat}`;
+	const searchQuotes = async (title) => {
+		const response = await fetch(url);
+		//GET /random
+		const data = await response.json();
+		console.log(data); //JSON results
+	};
+
+	//-----Search function-----
+	useEffect(() => {
+		searchQuotes('urlCat');
+	}, []);
+
+	//-----RETURN-----
+	return <div className='App'></div>;
+}
 export default App;

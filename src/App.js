8a2c4eae8c21b3336a7999e6bee75497f3cb96ad
Danny K <dxkalnz@gmail.com>
2022-10-05 11:36:49 -0600

    Create MUI branch, create legacy app, install mui deps

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-//App Object
-import { useEffect } from 'react';
+//~~~dk/dev~~~
+import React from 'react';
+import { useState, useEffect } from 'react';
+//import components
+
+//primary app function
 function App() {
-	//-----Fetch-----
-	// API REFERENCE - https://github.com/lukePeavey/quotable/blob/master/README.md#api-reference-
-	let currentQuote = null;
-	let quoteString = null;
-	let urlCat = 'random';
-	const url = `https://api.quotable.io/${urlCat}`;
-	const searchQuotes = async (title) => {
-		const response = await fetch(url);
-		//GET /random
-		const data = await response.json();
-		console.log(data); // <-------JSON results
-		//puts the JSON data into an object
-		currentQuote = {
-			text: data.content,
-			author: data.author,
-		};
-		//converts object key/value to a single string
-		quoteString = `${currentQuote.text}  --${currentQuote.author}`;
-		console.log(quoteString);
-	};
-
-	// -----Search function-----
-	useEffect(() => {
-		searchQuotes(urlCat);
-	}, []);
-
 	//-----RETURN-----
-	return (
-		<div className='App'>
-			<div className='quote' id='quoteText'>
-				{currentQuote.text}
-			</div>
-			<div className='quote' id='quoteAuthor'>
-				` --${currentQuote.author}`
-			</div>
-		</div>
-	);
+	return <div></div>;
 }
 export default App;

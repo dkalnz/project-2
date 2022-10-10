d866f74230dcf2deb8038ac58a581f945bdc0d57
Danny K <dxkalnz@gmail.com>
2022-10-10 12:05:45 -0600

    Create fetch inside useEffect for API call

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,19 +1,31 @@
 //This is the home page
-// function todaysQuote() {
-// 	const url = `https://api.quotable.io/tags`;
-// 	const searchQuotes = async (url) => {
-// 		const response = await fetch(url);
-// 		//GET /random
-// 		const myQuote = await response.json();
-// 		console.log(myQuote); //JSON results
-// 		return myQuote;
-// 	};
-// }
+//Link to API
+import { UseState, useEffect } from 'react';
 
 export default function Quotes() {
-	return (
-		<>
-			<div className='quotes-page'>MAIN Quotes Page</div>
-		</>
-	);
+	// return (
+	// 	<>
+	// 		<h2>Quote of the Day</h2>
+	// 		<p>This is the Quotes Page</p>
+	// 	</>
+	// );
+	useEffect(() => {
+  // declare the async data fetching function
+  const fetchData = async () => {
+    // get the data from the api
+    const data = await fetch('https://yourapi.com');
+    // convert data to json
+    const json = await data.json();
+    return json;
+  }
+
+  // call the function
+  const result = fetchData()
+    // make sure to catch any error
+    .catch(console.error);;
+
+  // ❌ don't do this, it won't work as you expect!
+  setData(result);
+}, [])random
+	};
 }
